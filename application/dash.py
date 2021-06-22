7b63adfbe0ef6e75010630bd4d25a43b36f7b835
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-06-22 12:19:27 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -971,7 +971,9 @@ body = html.Div([
 
 
     
-app.layout = html.Div([body])
+app.layout = html.Div([body],
+                              style={'width': '1900px',
+                                    "background-color": "lightgray"})
 
 from application.dash import app
 from settings import config
